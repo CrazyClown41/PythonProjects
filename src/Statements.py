e10e087691b18ed2da70f20a67511467a66c88dd
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-11-19 12:55:10 -0500

    commit 2

---
+++ src/Statements.py
@@ -0,0 +1,13 @@
+age = 18
+
+if age >= 18:
+    print("you are old too enough to smoke in PA")
+elif age >= 12:
+    print("you are a young adult")
+else:
+    print("you not are old too enough to smoke in PA")
+
+while age < 50:
+    print("not old enough, current age is"), age
+    age += 1
+
