fbcf54605c3c1b0731137bbef4852134853901cc
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-16 13:15:06 -0500

    commit 7

--- src/Statements.py
+++ src/Statements.py
@@ -1,10 +1,8 @@
-age = input("How old are you?")
+age = int(input("How old are you? "))
 
 if age >= 18:
-    print("you are old too enough to smoke in PA")
-elif age >= 12:
-    print("you are a young adult")
+    print("You are old enough to smoke in PA.")
 else:
-    print("you not are old too enough to smoke in PA")
+    print("You are not old enough to smoke in PA.")
 
 
