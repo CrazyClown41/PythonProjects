b636df1d0da1ff386a1893954ebbe7a64805ac6b
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-16 14:52:41 -0500

    commit 8

---
+++ src/Alternate.py
@@ -0,0 +1,12 @@
+def getNum():
+    num = int(input("Enter how many numbers you want: "))
+    return num
+
+def loop(num):
+    iterate = num % 2
+    for i in range(int(iterate)): 
+        print(0, end = " ")  
+        for j in range(1):
+            print(1, end = " ")
+
+loop(getNum())
