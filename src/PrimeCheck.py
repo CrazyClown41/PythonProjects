621ab6688e7adf6825154b23987105d64f27c35e
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-16 15:31:52 -0500

    commit 11

---
+++ src/PrimeCheck.py
@@ -0,0 +1,16 @@
+def getNum():
+    num = int(input("Enter number: "))
+    return num
+
+def primeCheck(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                break
+            else:
+                print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
+
+primeCheck(getNum())
