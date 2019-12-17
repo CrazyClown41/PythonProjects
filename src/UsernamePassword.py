ffe6d31208ae2117292ed538726fb11d089eaef4
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-16 20:53:20 -0500

    commit 15

--- src/UsernamePassword.py
+++ src/UsernamePassword.py
@@ -2,13 +2,6 @@ import tkinter as tk
 from tkinter import Entry, Label
 from datetime import datetime
 
-def writeToFile():
-    dateTime = datetime.time(datetime.now())
-    f = open("info.txt","w+")
-    f.write(e1.get())
-    f.write(e2.get())
-    f.close()
-
 window = tk.Tk()
 window.title ("Hello World")
 Label(window, text='Username:').grid(row=  0) 
@@ -17,6 +10,14 @@ e1 = Entry(window)
 e2 = Entry(window) 
 e1.grid(row=0, column=1) 
 e2.grid(row=1, column=1) 
+
+def writeToFile():
+    dateTime = datetime.time(datetime.now())
+    f = open("info.txt","w+")
+    f.write(e1.get())
+    f.write(e2.get())
+    f.close()
+
 button = tk.Button(window, text = 'Submit', width = 25, command = writeToFile()) 
 button.grid(row = 3, column = 1)
 button = tk.Button(window, text = 'Close', width = 25, command = window.destroy)
