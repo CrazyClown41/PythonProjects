0a77e79126dd9a1775b6c592c1fb5abaa7ee1e96
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-18 05:39:46 -0500

    commit 27

--- src/Web.py
+++ src/Web.py
@@ -1,7 +1,15 @@
-import webbrowser as web
 import tkinter as tk
+import webbrowser as web
 
+def openWeb():
+    web.open("https://www.github.com/CrazyClown41")
 
+window = tk.Tk()
+window.title ("Redirect to Github")
+button = tk.Button(window, text = "Open Github", width = 25, command = openWeb()) 
+button.grid(row = 1, column = 1)
+button = tk.Button(window, text = "Close", width = 25, command = window.destroy)
+button.grid(row = 2, column = 1)
     
         
-web.open("https://www.github.com/CrazyClown41")
+window.mainloop()
