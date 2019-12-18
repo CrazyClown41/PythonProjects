49c16ef26eea480f25a32b3819008fa75e34b483
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-18 05:03:04 -0500

    commit 24

--- src/ListLessThanTen.py
+++ src/ListLessThanTen.py
@@ -2,5 +2,18 @@ def getList():
     list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     return list1
 
-def returnLess():
-    for 
+def returnLessTen(list1):
+    list2 = []
+    for x in list1:
+        if x < 10:
+            list2.append(x)
+    return list2
+
+def returnLessFive(list2):
+    list3 = []
+    for x in list2: 
+        if x < 5:
+            list3.append(x)
+    print(list3)
+
+returnLessFive(returnLessTen(getList()))
