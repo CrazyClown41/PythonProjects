0a546ae5cd4bab3dbd3d06075489dc529256dc6f
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-12-17 22:48:55 -0500

    commit 17

---
+++ src/GuessNumber.py
@@ -0,0 +1,24 @@
+import random
+
+def getNum():
+    num = int(input("Guess a number from 1 - 25: "))
+    if num <= 25 and num >= 1:
+        return num
+    else:
+        print("The number you entered is not between 1 and 25.")
+
+def randomNum():
+    randomNum = random.randint(1, 25)
+    return randomNum
+
+def guess(randomNum, num):
+    if num == randomNum:
+        print("You guessed the right number!")
+    elif num < randomNum:
+        print("Your number is too low. Guess again!")
+        guess(randomNum(),getNum())
+    elif num > randomNum:
+        print("Your number is too high. Guess again!")
+        guess(randomNum(),getNum())
+
+guess(randomNum(),getNum())
